0b73044a4c3182632140f8dec82001c40086c00b
Yumiko Shiga <totosmy2718@gmail.com>
2026-08-24 17:53:13 +0800

    feat: 公開API実装 #7

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
+
+// 公開API（認証不要）
+Route::apiResource('tasks', TaskController::class)->only(['index', 'show']);
